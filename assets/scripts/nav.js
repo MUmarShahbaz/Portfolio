bee57ad4ea125c6e24309da7a4f1d4d045140ed1
MUmarShahbaz <MUmarShahbaz@users.noreply.github.com>
2025-04-21 01:32:42 +0000

    Deploying to gh-pages from @ MUmarShahbaz/Portfolio@819aae4b81793d0c5633c72f49bde562589a22c9 🚀

---
+++ assets/scripts/nav.js
@@ -0,0 +1,33 @@
+addEventListener("DOMContentLoaded", () => {
+  const navBG = [document.getElementById("nav-bar"), document.getElementById("menu-bg")];
+  const progressbars = document.querySelectorAll(".progress");
+  const links = document.getElementById("nav-links");
+  const menu = document.getElementById("menu");
+  const progressbar = document.getElementById("live-progress");
+
+  menu.addEventListener("click", () => {
+    links.style.top = links.style.top === "82px" ? "-711px" : "82px";
+  });
+
+  this.window.addEventListener("scroll", () => {
+    const percentScrolled = (window.scrollY / (document.body.offsetHeight - window.innerHeight)) * 100;
+    progressbar.style.width = `${percentScrolled}%`
+  });
+
+  let lastScroll = 0;
+  window.addEventListener("scroll", () => {
+    const currentScroll = window.scrollY;
+    if (currentScroll > lastScroll + 30) {
+      navBG.forEach((element) => {
+        element.style.top = "-80px"; });
+      progressbars.forEach((element) => {
+        element.style.top = "0px"; });
+    } else if (currentScroll < lastScroll - 30) {
+      navBG.forEach((element) => {
+        element.style.top = "0"; });
+      progressbars.forEach((element) => {
+        element.style.top = "80px"; });
+    }
+    lastScroll = currentScroll;
+  });
+});
