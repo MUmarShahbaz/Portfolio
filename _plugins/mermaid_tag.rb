47637d0f62655a2d194583a782075e7cbc3852c8
M. Umar Shahbaz <m.umarshahbaz.2007@gmail.com>
2025-09-05 10:32:51 +0500

    Added Dark Mode
    Restructured Code Block Styles
    Added Dual-Color Elements styles
    Made Repo Cards and Mermaid Dual Color
    Added Theme Toggle

--- _plugins/mermaid_tag.rb
+++ _plugins/mermaid_tag.rb
@@ -6,7 +6,7 @@ module Jekyll
     end
 
     def render(context)
-      "<div class=\"mermaid w-100 d-flex justify-content-center\">#{super}</div>"
+      "<div class=\"dual-color-container\"><div class=\"mermaid dual-color-light w-100 d-flex justify-content-center\">#{super}</div><div class=\"mermaid dual-color-dark w-100 d-flex justify-content-center\">%%{init: {\"theme\": \"dark\"}}%%#{super}</div></div>"
     end
   end
 end
